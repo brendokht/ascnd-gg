69adc8e615466323f3c745ede9b655c19b5ff47b
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2025-08-21 13:17:17 -0400

    Initial commit

---
+++ packages/eslint-config/README.md
@@ -0,0 +1,3 @@
+# `@ascnd-gg/eslint-config`
+
+Collection of internal eslint configurations.
---
+++ packages/ui/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from "@ascnd-gg/eslint-config/react-internal";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
