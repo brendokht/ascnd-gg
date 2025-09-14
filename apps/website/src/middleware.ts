9a89b15e6e30c0b4573128fe039ce34cd65cfb1a
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2025-09-14 04:07:31 -0400

    Updated middleware to look for specific routes for protection

--- apps/website/src/middleware.ts
+++ apps/website/src/middleware.ts
@@ -1,16 +1,36 @@
 import { getSessionCookie } from "better-auth/cookies";
 import { NextResponse, NextRequest } from "next/server";
+import { protectedPathnames } from "./lib/protected-routes";
 
 export async function middleware(request: NextRequest) {
   console.log("Middleware: Started");
 
   const response = NextResponse.next({ request });
 
-  if (request.nextUrl.pathname.startsWith("/protected")) {
+  console.log(
+    "Middleware: `request.nextUrl.pathname` is",
+    request.nextUrl.pathname,
+  );
+
+  /*
+   * Get the first part of the pathname
+   * Ex. `/protected/dashboard` -> `/protected`
+   * or: `/settings` -> `/settings`
+   */
+  const path = request.nextUrl.pathname.split("/")[1]
+    ? `/${request.nextUrl.pathname.split("/")[1]}`
+    : "/";
+
+  console.log("Middleware: Path is", path || "empty");
+
+  if (protectedPathnames.includes(path)) {
+    console.log("Middleware: Protecting");
     const sessionCookie = getSessionCookie(request);
 
     if (!sessionCookie) {
-      return NextResponse.redirect(new URL("/?unauthorized=true", request.url));
+      return NextResponse.redirect(
+        new URL("/sign-in?unauthorized=true", request.url),
+      );
     }
   }
 
@@ -18,7 +38,6 @@ export async function middleware(request: NextRequest) {
 
   return response;
 }
-// TODO: Ensure session can be refreshed in morning, continue with auth context/provider
 // TODO: Set up React Query; Ensure client side and server fetching utility is set up
 export const config = {
   matcher: [
