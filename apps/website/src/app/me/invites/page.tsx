0be3be875bef029658f6702c8197af78dde4670b
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2025-10-10 01:24:26 -0400

    Updated current user invites page to fetch hub invites, updated fetching logic, and added re-fetch in case of error.

--- apps/website/src/app/me/invites/page.tsx
+++ apps/website/src/app/me/invites/page.tsx
@@ -1,4 +1,7 @@
-import { type TeamInviteForUserViewModel } from "@ascnd-gg/types";
+import {
+  HubInviteForUserViewModel,
+  type TeamInviteForUserViewModel,
+} from "@ascnd-gg/types";
 import { fetchApi } from "@ascnd-gg/website/lib/fetch-utils";
 import { headers } from "next/headers";
 import InvitesList from "./invites-list";
@@ -9,13 +12,38 @@ export default async function Invites() {
 
   const user = session.user;
 
-  // TODO: Fetch event and hub invites using Promise.allSettled
-  const { data: teamInvites, error } = await fetchApi<
-    Array<TeamInviteForUserViewModel>
-  >("/me/teams/invites", await headers());
+  const teamInvitesPromise = fetchApi<Array<TeamInviteForUserViewModel>>(
+    "/me/teams/invites",
+    await headers(),
+  );
 
-  if (error) {
-    return <>Something went wrong</>;
+  const hubInvitesPromise = fetchApi<Array<HubInviteForUserViewModel>>(
+    "/me/hubs/invites",
+    await headers(),
+  );
+
+  const [teamInvitesResult, hubInvitesResult] = await Promise.allSettled([
+    teamInvitesPromise,
+    hubInvitesPromise,
+  ]);
+
+  let teamInvites: Array<TeamInviteForUserViewModel> | null = null;
+  let hubInvites: Array<HubInviteForUserViewModel> | null = null;
+
+  if (teamInvitesResult?.status === "fulfilled") {
+    const { data: teamInvitesData, error: teamInvitesError } =
+      teamInvitesResult.value;
+
+    if (teamInvitesError) teamInvites = null;
+    else teamInvites = teamInvitesData ?? [];
+  }
+
+  if (hubInvitesResult?.status === "fulfilled") {
+    const { data: hubInvitesData, error: hubInvitesError } =
+      hubInvitesResult.value;
+
+    if (hubInvitesError) hubInvites = null;
+    else hubInvites = hubInvitesData ?? [];
   }
 
   return (
@@ -27,7 +55,11 @@ export default async function Invites() {
           hubs. You can accept or decline the invites you have received.
         </p>
       </div>
-      <InvitesList currentUserId={user.id} teamInvites={teamInvites ?? []} />
+      <InvitesList
+        currentUserId={user.id}
+        teamInvitesData={teamInvites}
+        hubInvitesData={hubInvites}
+      />
     </>
   );
 }
