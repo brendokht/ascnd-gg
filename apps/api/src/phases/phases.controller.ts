cb16d6b92375c9e44fa037e14bbca6d8bbe8ffff
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2025-11-10 02:51:20 -0500

    Updated phases controller and service, added schemas.

--- apps/api/src/phases/phases.controller.ts
+++ apps/api/src/phases/phases.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from "@nestjs/common";
+import { PhasesService } from "./phases.service";
+import {
+  CreatePhaseDto,
+  EditPhaseDto,
+  PhaseIdParameterDto,
+  PhaseViewModel,
+} from "@ascnd-gg/types";
+import { User } from "@ascnd-gg/database";
+import { Optional } from "../auth/auth.decorator";
 
-@Controller('phases')
-export class PhasesController {}
+@Controller("phases")
+export class PhasesController {
+  constructor(private readonly phaseService: PhasesService) {}
+
+  @Get("/:phaseId")
+  @Optional()
+  async getPhaseById(
+    @Req() req: Request,
+    @Param() params: PhaseIdParameterDto,
+  ): Promise<PhaseViewModel> {
+    const phase = await this.phaseService.getPhaseById(params);
+
+    if (!phase) {
+      throw new NotFoundException();
+    }
+
+    return phase;
+  }
+
+  @Post()
+  async createPhase(
+    @Req() req: Request,
+    @Body() createPhaseDto: CreatePhaseDto,
+  ): Promise<PhaseViewModel> {
+    const createdPhase = await this.phaseService.createPhase(
+      req["user"] as User,
+      createPhaseDto,
+    );
+
+    return createdPhase;
+  }
+
+  @Patch(":phaseId")
+  async updatePhase(
+    @Req() req: Request,
+    @Param() params: PhaseIdParameterDto,
+    @Body() editPhaseDto: EditPhaseDto,
+  ): Promise<Partial<PhaseViewModel>> {
+    const updatedPhase = await this.phaseService.updatePhase(
+      req["user"] as User,
+      params,
+      editPhaseDto,
+    );
+
+    return updatedPhase;
+  }
+}
