bf712fcbe2a47da42da5f54ce8b44ea3662ce1cc
Brenden Kohut <103231108+brendokht@users.noreply.github.com>
2025-12-14 06:26:51 -0500

    Small config fix for images

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -5,10 +5,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { hostname: "lh3.googleusercontent.com" },
-      { hostname: "localhost" },
+      { hostname: "localhost", port: "9000", protocol: "http" },
       { hostname: "placehold.co" },
     ],
     qualities: [25, 50, 75, 100],
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
   },
   experimental: {
     browserDebugInfoInTerminal: true,
